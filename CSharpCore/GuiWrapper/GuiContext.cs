aafd813147ec7384797f4f0bb2755f03e0ef13b8
johnsonsong <1832617@qq.com>
2020-10-28 17:41:52 +0800

    try to import ImGui for editor

---
+++ CSharpCore/GuiWrapper/GuiContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ImGuiNET;
+
+namespace EngineNS.GuiWrapper
+{
+    public class GuiContext
+    {
+        public GuiContext()
+        {
+            mContext = ImGui.CreateContext();
+        }
+        ~GuiContext()
+        {
+            if (mContext != IntPtr.Zero)
+            {
+                ImGui.DestroyContext(mContext);
+                mContext = IntPtr.Zero;
+            }
+        }
+        IntPtr mContext;
+        public IntPtr Context
+        {
+            get { return mContext; }
+        }
+        public void SetFont(RName fontname)
+        {
+            var fonts = ImGui.GetIO().Fonts;
+            fonts.AddFontDefault();
+
+            FontTexId = GuiManager.Instance.GetFontMaterial(fontname, this).Ptr;
+            fonts.SetTexID(FontTexId);
+        }
+        public IntPtr FontTexId
+        {
+            get;
+            private set;
+        }
+        public void TickLogic(float deltaSeconds)
+        {
+            ImGui.SetCurrentContext(mContext);
+            SetPerFrameImGuiData(deltaSeconds);
+            ImGui.NewFrame();
+        }
+        public Vector2 ScaleFactor
+        {
+            get;
+            set;
+        }
+        private void SetPerFrameImGuiData(float deltaSeconds)
+        {
+            ImGuiIOPtr io = ImGui.GetIO();
+            //io.DisplaySize = new Vector2(
+            //    _windowWidth / _scaleFactor.X,
+            //    _windowHeight / _scaleFactor.Y);
+            //io.DisplayFramebufferScale = new System.Numerics.Vector2(ScaleFactor.X, ScaleFactor.Y);
+            io.DeltaTime = deltaSeconds; // DeltaTime is in seconds.
+        }
+    }
+}
---
+++ CSharpCore/GuiWrapper/GuiManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineNS.GuiWrapper
+{
+    public struct THandle<T> where T : class
+    {
+        private System.Runtime.InteropServices.GCHandle Handle;
+        public THandle(T obj) 
+        {
+            Handle = System.Runtime.InteropServices.GCHandle.Alloc(obj);
+        }
+        public THandle(IntPtr p)
+        {
+            Handle = System.Runtime.InteropServices.GCHandle.FromIntPtr(p);
+        }
+        public void Free()
+        {
+            Handle.Free();
+        }
+        public IntPtr Ptr
+        {
+            get
+            {
+                return System.Runtime.InteropServices.GCHandle.ToIntPtr(Handle);
+            }
+        }
+        public T Get()
+        {
+            return Handle.Target as T;
+        }
+    }
+
+    public class GuiManager
+    {
+        public static GuiManager Instance = new GuiManager();
+        public void Cleanup()
+        {
+            foreach(var i in AllMaterials)
+            {
+                i.Value.Free();
+            }
+            AllMaterials.Clear();
+        }
+        public async System.Threading.Tasks.Task<THandle<Graphics.CGfxMaterialInstance>> GetMaterialRHI(RName name)
+        {
+            THandle<Graphics.CGfxMaterialInstance> result;
+            if (AllMaterials.TryGetValue(name, out result))
+                return result;
+
+            var rsv = await CEngine.Instance.MaterialInstanceManager.GetMaterialInstanceAsync(CEngine.Instance.RenderContext, name);
+            result = new THandle<Graphics.CGfxMaterialInstance>(rsv);
+            AllMaterials.Add(name, result);
+            return result;
+        }
+        private Dictionary<RName, THandle<Graphics.CGfxMaterialInstance>> AllMaterials = new Dictionary<RName, THandle<Graphics.CGfxMaterialInstance>>();
+        public THandle<Graphics.CGfxMaterialInstance> GetFontMaterial(RName fontname, GuiContext context)
+        {
+            THandle<Graphics.CGfxMaterialInstance> result;
+            if (AllMaterials.TryGetValue(fontname, out result))
+                return result;
+            //从ImGui中获得TextAtlas数据，创建纹理，然后创建材质
+
+            AllMaterials.Add(fontname, result);
+            return result;
+        }
+    }
+}
