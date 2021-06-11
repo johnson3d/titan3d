c179e8d3c1043c9a7d391aa1e25e5a9a58bd8168
Sunqifeng <361248816@qq.com>
2021-06-11 22:54:00 +0800

    Minor

---
+++ CSharpCode/Bricks/Animation/Skeleton/Limb/ILimb.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineNS.Animation.Skeleton
+{
+    public struct IndexInSkeleton
+    {
+        public static IndexInSkeleton Default { get; } = new IndexInSkeleton(-1);
+        public IndexInSkeleton(int index)
+        {
+            Value = index;
+        }
+        public int ToInt()
+        {
+            return Value;
+        }
+        public bool IsValid()
+        {
+            return Value != -1 ? true : false;
+        }
+        private int Value;
+    }
+}
+
+namespace EngineNS.Animation.Skeleton.Limb
+{
+    public interface ILimbDesc : IO.ISerializer
+    {
+        [Rtti.Meta]
+        VNameString Name { get; set; }
+        [Rtti.Meta]
+        public uint NameHash { get; set; }
+        [Rtti.Meta]
+        public VNameString ParentName { get; set; }
+        [Rtti.Meta]
+        public uint ParentHash { get; set; } 
+        [Rtti.Meta]
+        public EngineNS.Matrix InitMatrix { get; set; }
+    }
+    public interface ILimb : IO.ISerializer
+    {
+        public ILimbDesc Desc { get; }
+        [Rtti.Meta]
+        public List<ILimb> Children { get; }
+        public IndexInSkeleton ParentIndex { get; set; }
+        public IndexInSkeleton Index { get; set; }
+    }
+}
