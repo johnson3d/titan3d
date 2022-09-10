e39863e5891c8348441fd66989a93407a19c6b36
johnsonsong <johnsonsong@tencent.com>
2022-09-10 20:51:27 +0800

    delete

--- NativeCode/RHI/D3D11/D11PreHead.cpp
+++
@@ -1,9 +0,0 @@
-#include "D11PreHead.h"
-
-#pragma comment(lib,"DXGI.lib")
-#pragma comment(lib,"D3D11.lib")
-#pragma comment(lib,"d3dcompiler.lib")
-#pragma comment(lib,"dxguid.lib")
-
-#define new VNEW
-
