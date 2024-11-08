3d1a3efcb101d929d8ff9ab1ea6444fc5da8e829
johnson3d <1832617@qq.com>
2024-11-08 10:28:16 +0800

    update

--- CSharpCode/Bricks/FX/Hair/HairShader.cs
+++ CSharpCode/Bricks/FX/Hair/HairShader.cs
@@ -5,6 +5,7 @@ using EngineNS.Graphics.Pipeline.Shader;
 using EngineNS.Bricks.CodeBuilder;
 using EngineNS.Bricks.CodeBuilder.ShaderNode;
 using EngineNS.Bricks.CodeBuilder.ShaderNode.Control;
+using NPOI.SS.Formula.Functions;
 
 
 namespace EngineNS.Bricks.FX.Hair
@@ -13,6 +14,14 @@ namespace EngineNS.Bricks.FX.Hair
     [TtMaterialShader]
     public partial class TtHairShader
     {
+        [Rtti.Meta]
+        [TtMaterialShader(Name = "ShiftTangent", Include = "@Engine/Shaders/Bricks/FX/Hair.cginc")]
+        [ContextMenu("ShiftTangent", "FX\\ShiftTangent", TtMaterialGraph.MaterialEditorKeyword)]
+        public static Vector3 ShiftTangent(Vector3 T, Vector3 N, float shift)
+        {
+            Vector3 shiftedT = T + (shift * N);
+            return Vector3.Normalize(shiftedT);
+        }
         [Rtti.Meta]
         [TtMaterialShader(Name = "StrandSpecular", Include = "@Engine/Shaders/Bricks/FX/Hair.cginc")]
         [ContextMenu("StrandSpecular", "FX\\StrandSpecular", TtMaterialGraph.MaterialEditorKeyword)]
@@ -25,22 +34,44 @@ namespace EngineNS.Bricks.FX.Hair
             return dirAtten * MathF.Pow(sinTH, exponent);
         }
         [Rtti.Meta]
-        [TtMaterialShader(Name = "ShiftTangent", Include = "@Engine/Shaders/Bricks/FX/Hair.cginc")]
-        [ContextMenu("ShiftTangent", "FX\\ShiftTangent", TtMaterialGraph.MaterialEditorKeyword)]
-        public static Vector3 ShiftTangent(Vector3 T, Vector3 N, float shift)
+        [TtMaterialShader(Name = "GetAnisotropicNeubelt", Include = "@Engine/Shaders/Bricks/FX/Hair.cginc")]
+        [ContextMenu("GetAnisotropicNeubelt", "FX\\GetAnisotropicNeubelt", TtMaterialGraph.MaterialEditorKeyword)]
+        public static void GetAnisotropicNeubelt(float roughness, float anisotropic, out float ax, out float az)
         {
-            Vector3 shiftedT = T + (shift * N);
-            return Vector3.Normalize(shiftedT);
+            float roughnessSq = roughness * roughness;
+            ax = roughnessSq;
+            az = MathHelper.Lerp(0, roughnessSq, 1 - anisotropic);
         }
         [Rtti.Meta]
-        [TtMaterialShader(Name = "GetAnisotropicRoughness", Include = "@Engine/Shaders/Bricks/FX/Hair.cginc")]
-        [ContextMenu("GetAnisotropicRoughness", "FX\\GetAnisotropicRoughness", TtMaterialGraph.MaterialEditorKeyword)]
-        public static void GetAnisotropicRoughness(float roughness, float anisotropic, out float ax, out float az)
+        [TtMaterialShader(Name = "GetAnisotropicBurley", Include = "@Engine/Shaders/Bricks/FX/Hair.cginc")]
+        [ContextMenu("GetAnisotropicBurley", "FX\\GetAnisotropicBurley", TtMaterialGraph.MaterialEditorKeyword)]
+        public static void GetAnisotropicBurley(float roughness, float anisotropic, out float ax, out float az)
         {
             float aspect = MathF.Sqrt(1.0f - 0.9f * anisotropic);
             float roughnessSq = roughness * roughness;
             ax = roughnessSq / aspect;
             az = roughnessSq * aspect;
+        }
+        [Rtti.Meta]
+        [TtMaterialShader(Name = "GetAnisotropicKulla", Include = "@Engine/Shaders/Bricks/FX/Hair.cginc")]
+        [ContextMenu("GetAnisotropicKulla", "FX\\GetAnisotropicKulla", TtMaterialGraph.MaterialEditorKeyword)]
+        public static void GetAnisotropicKulla(float roughness, float anisotropic, out float ax, out float az)
+        {
+            float aspect = MathF.Sqrt(1.0f - 0.9f * anisotropic);
+            float roughnessSq = roughness * roughness;
+            ax = roughnessSq / aspect;
+            az = roughnessSq * aspect;
+        }
+        [Rtti.Meta]
+        [TtMaterialShader(Name = "D_Beckmann_aniso", Include = "@Engine/Shaders/Bricks/FX/Hair.cginc")]
+        [ContextMenu("D_Beckmann_aniso", "FX\\D_Beckmann_aniso", TtMaterialGraph.MaterialEditorKeyword)]
+        public static float D_Beckmann_aniso(float ax, float az, float NoH, Vector3 H, Vector3 T, Vector3 B)
+        {
+            float ToH = Vector3.Dot(T, H);
+            float BoH = Vector3.Dot(B, H);
+            float NoH_Sq = NoH * NoH;
+            float d = -(ToH * ToH / (ax * ax) + BoH * BoH / (az * az)) / (NoH_Sq);
+            return MathF.Exp(d) / (MathF.PI * ax * az * NoH_Sq * NoH_Sq);
         }
         [Rtti.Meta]
         [TtMaterialShader(Name = "D_GGXaniso", Include = "@Engine/Shaders/Bricks/FX/Hair.cginc")]
--- EngineContent/Shaders/Bricks/FX/Hair.cginc
+++ EngineContent/Shaders/Bricks/FX/Hair.cginc
@@ -2,14 +2,13 @@
 #define __FX_HAIR_H__
 #include "../../Inc/SysFunction.cginc"
 
-//https://blog.csdn.net/qjh5606/article/details/118117176
-
 float3 ShiftTangent(float3 T, float3 N, float shift)
 {
     float3 shiftedT = T + (shift * N);
     return normalize(shiftedT);
 }
 
+//https://zhuanlan.zhihu.com/p/43248692
 float StrandSpecular(float3 T, float3 V, float3 L, float exponent)
 {
     float3 H = normalize(L + V);
@@ -19,12 +18,36 @@ float StrandSpecular(float3 T, float3 V, float3 L, float exponent)
     return dirAtten * pow(sinTH, exponent);
 }
 
-void GetAnisotropicRoughness(float roughness, float anisotropic, out float ax, out float az)
+//https://blog.csdn.net/qjh5606/article/details/118117176
+void GetAnisotropicNeubelt(float roughness, float anisotropic, out float ax, out float az)
+{
+    float roughnessSq = roughness * roughness;
+    ax = roughnessSq;
+    az = lerp(0, roughnessSq, 1 - anisotropic);
+}
+
+void GetAnisotropicBurley(float roughness, float anisotropic, out float ax, out float az)
 {
     float aspect = sqrt(1.0 - 0.9 * anisotropic);
     float roughnessSq = roughness * roughness;
     ax = roughnessSq / aspect;
     az = roughnessSq * aspect;
+}
+
+void GetAnisotropicKulla(float roughness, float anisotropic, out float ax, out float az)
+{
+    float roughnessSq = roughness * roughness;
+    ax = roughnessSq * (1 - anisotropic);
+    az = roughnessSq * (1 + anisotropic);
+}
+
+float D_Beckmann_aniso(float ax, float az, float NoH, float3 H, float3 T, float3 B)
+{
+    float ToH = dot(T, H);
+    float BoH = dot(B, H);
+    float NoH_Sq = NoH * NoH;
+    float d = -(ToH * ToH / (ax * ax) + BoH * BoH / (az * az)) / (NoH_Sq);
+    return exp(d) / (PI * ax * az * NoH_Sq * NoH_Sq);
 }
 
 // Anisotropic GGX
