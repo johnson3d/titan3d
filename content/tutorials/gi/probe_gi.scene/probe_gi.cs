b8eb71fc88db82f73ec63c8d21e5f08cd95b91c4
johnson3d <1832617@qq.com>
2025-09-03 14:32:27 +0800

    update

---
+++ content/tutorials/gi/probe_gi.scene/probe_gi.cs
@@ -0,0 +1,15 @@
+﻿namespace NS_tutorials.gi
+{
+    [EngineNS.Macross.TtMacross]
+    [EngineNS.Macross.TtMacrossSign(RName_Name = "tutorials/gi/probe_gi.scene", RName_Type = EngineNS.RName.ERNameType.Game)]
+    public partial class probe_gi : EngineNS.GamePlay.Scene.TtSceneMacrossBase
+    {
+        EngineNS.Macross.TtMacrossStackFrame mFrame_InitializeMacrossNodePropertyValues_3642530370 = new EngineNS.Macross.TtMacrossStackFrame(EngineNS.RName.GetRName("tutorials/gi/probe_gi.scene", EngineNS.RName.ERNameType.Game));
+        public override void InitializeMacrossNodePropertyValues()
+        {
+            using(var guard_InitializeMacrossNodePropertyValues = new EngineNS.Macross.TtMacrossStackGuard(mFrame_InitializeMacrossNodePropertyValues_3642530370))
+            {
+            }
+        }
+    }
+}
